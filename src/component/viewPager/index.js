8ef28c255d8c08e4e51e5c267c5f83139e829a50
lear <1325925707@qq.com>
2018-04-08 14:44:44 +0800

    Optimization code

--- src/component/viewPager/index.js
+++ src/component/viewPager/index.js
@@ -9,7 +9,6 @@ import {
   View,
   PanResponder,
   Animated,
-  StyleSheet,
   Easing,
   NativeModules,
   Platform
@@ -22,17 +21,16 @@ import ViewPagerDataSource from './ViewPagerDataSource';
 const deviceWidth = Dimensions.get('window').width;
 const LeftBoundary = deviceWidth / 4;
 const RightBoundary = deviceWidth - LeftBoundary;
-let Shield = 0;//修复呼出菜单之后下一次滑页出现bug
-const Brightness = Platform.OS === 'ios' && NativeModules.Brightness;
 
+const Brightness = Platform.OS === 'ios' && NativeModules.Brightness;
 
 class ViewPager extends PureComponent {
   static DataSource = ViewPagerDataSource;
   constructor(props) {
     super(props);
     this.fling = false;
-    this.isLoop = false;
-    this.locked = false;
+    this.shield = 0;//修复呼出菜单之后下一次滑页出现bug
+    this.locked = false; // 锁屏的，当菜单呼出之时就锁住屏幕
     this.animation = (animate, toValue, gs) => {
       return Animated.timing(animate,
         {
@@ -42,22 +40,21 @@ class ViewPager extends PureComponent {
           useNativeDriver: true,//使用原生驱动，更加流畅
         });
     };
-    let maxP;
     Platform.OS === 'ios' && Brightness.get(x => {
       this.brightVal = +x;//转型
     })
     this.shouldJmp = true;
+    this.toprev = 0;
     this.state = {
-      toprev: 0,
       currentPage: 0,
       viewWidth: 0,
       scrollValue: new Animated.Value(0)
     };
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this.childIndex = 0;
-    this.maxP = this.props.dataSource.getPageCount();
+    this.maxPage = this.props.dataSource.getPageCount();  // 最大页数，总页数
     let release = (e, gestureState) => {
       if (!this.shouldJmp) {
         this.shouldJmp = true;
@@ -65,7 +62,6 @@ class ViewPager extends PureComponent {
       };
       let relativeGestureDistance = gestureState.dx / deviceWidth,
         vx = gestureState.vx;
-      Shield = Shield >= 2 ? 0 : Shield;
       let step = 0;
       if (relativeGestureDistance < -0.5 || (relativeGestureDistance < 0 && vx <= -1e-6)) {
         step = 1;
@@ -76,43 +72,50 @@ class ViewPager extends PureComponent {
        *   x0 是x轴的起始位置
        *   moveX 是x轴的结束滑动位置，如果没滑就是0
        */
-      let clickX = gestureState.x0;
-      let moveX = gestureState.dx;
-      let flag = gestureState.moveX === 0 ? 0 : (gestureState.moveX > gestureState.x0 ? -1 : 1);
+      const clickX = gestureState.x0;
+      const moveX = gestureState.dx;
+      const flag = gestureState.moveX === 0 ? 0 : (gestureState.moveX > gestureState.x0 ? -1 : 1);
       if (clickX > LeftBoundary && clickX < RightBoundary && moveX == 0) {
         this.props.clickBoard();
-        Shield++;
+        this.shield++;
         return;
       }
 
       if (this.props.locked) return false;
 
-      if (clickX > RightBoundary && moveX == 0 || flag === 1) {
-        this.props.hasTouch && this.props.hasTouch(false);
-        this.setState({ toprev: 0 }, () => {
-          this.movePage(1, gestureState, moveX !== 0);//moveX !== 0 这里是判断是否启用动画效果
-          return;
-        });
-      } else if (clickX < LeftBoundary && moveX == 0 || flag === -1) {
-        this.props.hasTouch && this.props.hasTouch(false);
-        this.setState({ toprev: 1 }, () => {
-          this.movePage(-1, gestureState, moveX !== 0);
-          return;
-        });
-      }
-
       this.props.hasTouch && this.props.hasTouch(false);
 
+      if (clickX > RightBoundary && moveX == 0 || flag === 1) {
+        this.toprev = 0;
+        let animateFlag = moveX !== 0;
+        if (this.selfLock) {
+          this.selfLock = false;
+          animateFlag = false;
+          this.shield = 0;
+        }
+
+        this.movePage(1, gestureState, animateFlag);//moveX !== 0 这里是判断是否启用动画效果
+        return;
+      } else if (clickX < LeftBoundary && moveX == 0 || flag === -1) {
+        this.toprev = 1;
+        let animateFlag = moveX !== 0;
+        if (this.selfLock) {
+          this.selfLock = false;
+          animateFlag = false;
+          this.shield = 0;
+        }
+        this.movePage(-1, gestureState, animateFlag);
+        return;
+      }
       this.movePage(step, gestureState);
     };
 
 
     this._panResponder = PanResponder.create({
 
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (e, gestureState) => true,
       // Claim responder if it's a horizontal pan
       onMoveShouldSetPanResponder: (e, gestureState) => {
-        // console.log(this.props.locked);
         if (Math.abs(gestureState.dx) > Math.abs(gestureState.dy)) {
           if (this.props.locked !== true && !this.fling) {
             this.props.hasTouch && this.props.hasTouch(true);
@@ -120,7 +123,6 @@ class ViewPager extends PureComponent {
           }
         }
       },
-
       // Touch is released, scroll to the one that you're closest to
       onPanResponderRelease: release,
 
@@ -129,7 +131,8 @@ class ViewPager extends PureComponent {
       // Dragging, move the view with the touch
       onPanResponderMove: (e, gestureState) => {
         if (this.props.locked) return false;
-        if (Shield >= 2) {
+        if (this.shield > 0 && this.shield % 2 === 0) {
+          this.selfLock || (this.selfLock = true);
           return;
         }
         let finger = gestureState.numberActiveTouches;
@@ -145,7 +148,7 @@ class ViewPager extends PureComponent {
             if (this.brightVal < 0.0) {
               this.brightVal = 0.0;
             }
-            requestAnimationFrame(()=>{
+            requestAnimationFrame(() => {
               Brightness.set(this.brightVal);
             });
           } else if (this.brightVal !== 1.0 && moveY < 0.0) { //上滑
@@ -153,7 +156,7 @@ class ViewPager extends PureComponent {
             if (this.brightVal > 1.0) {
               this.brightVal = 1.0;
             }
-            requestAnimationFrame(()=>{
+            requestAnimationFrame(() => {
               Brightness.set(this.brightVal);
             });
           }
@@ -162,10 +165,6 @@ class ViewPager extends PureComponent {
       },
     });
 
-    if (this.props.isLoop) {
-      this.childIndex = 1;
-      this.state.scrollValue.setValue(1);
-    }
     let initialPage = Number(this.props.initialPage);
     if (initialPage > 0) {
       this.goToPage(initialPage, false);
@@ -177,45 +176,29 @@ class ViewPager extends PureComponent {
     if (this.props.Gpag == 1) {
       this.goToPage(0, false);
     } else if (this.props.Gpag == -1) {
-      this.goToPage(this.maxP - 1, false);
+      this.goToPage(this.maxPage - 1, false);
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.dataSource) {
-      let maxPage = nextProps.dataSource.getPageCount();
-      this.maxP = maxPage;
-      let constrainedPage = Math.max(0, Math.min(this.state.currentPage, maxPage));
-      this.setState({
-        currentPage: constrainedPage,
-      });
-
-      if (!nextProps.isLoop) {
-        this.state.scrollValue.setValue(constrainedPage > 0 ? 1 : 0);
-      }
       this.childIndex = 0;
-      this.fling = false;
     }
+    return false;
   }
 
   goToPage(pageNumber, animate = true) {
-
-    let pageCount = this.maxP;
-    if (pageNumber < 0 || pageNumber > pageCount) {
-      return;
+    if (pageNumber >= 0 && pageNumber <= this.maxPage) {
+      this.movePage(pageNumber - this.state.currentPage, null, animate);
     }
-
-    let step = pageNumber - this.state.currentPage;
-    this.movePage(step, null, animate);
   }
 
   movePage(step, gs, animate = true) {
-    let pageCount = this.maxP;
+    let pageCount = this.maxPage;
     let pageNumber = this.state.currentPage + step;
 
-
     //私人修改
-    if (pageNumber >= pageCount && this.state.toprev == 0) {
+    if (pageNumber >= pageCount && this.toprev == 0) {
       let tmpag = pageNumber;
       pageNumber = 0;
       if (this.props.getNextPage() === -1) {
@@ -223,19 +206,15 @@ class ViewPager extends PureComponent {
       }
       this.props.getCurrentPage(pageNumber + 1);
       return;
-    } else if (pageNumber < 0 && this.state.toprev == 1) {
+    } else if (pageNumber < 0 && this.toprev == 1) {
       this.props.getPrevPage();
       return;
     }
     step !== 0 && this.props.getCurrentPage(pageNumber + 1);
-    if (this.props.isLoop) {
-      pageNumber = pageCount == 0 ? pageNumber = 0 : ((pageNumber + pageCount) % pageCount);
-    } else {
-      pageNumber = Math.min(Math.max(0, pageNumber), pageCount - 1);
-    }
+    pageNumber = Math.min(Math.max(0, pageNumber), pageCount - 1);
     const moved = pageNumber !== this.state.currentPage;
     const scrollStep = (moved ? step : 0) + this.childIndex;
-    const nextChildIdx = (pageNumber > 0 || this.props.isLoop) ? 1 : 0;
+    const nextChildIdx = pageNumber > 0 ? 1 : 0;
     const postChange = () => {
       this.fling = false;
       this.childIndex = nextChildIdx;
@@ -260,16 +239,12 @@ class ViewPager extends PureComponent {
     }
   }
 
-  getCurrentPage() {
-    return this.state.currentPage;
-  }
-
-  _getPage(pageIdx, loop = false) {
+  _getPage(pageIdx) {
     let dataSource = this.props.dataSource;
     let pageID = dataSource.pageIdentities[pageIdx];
     return (
       <StaticRenderer
-        key={'p_' + pageID + (loop ? '_1' : '')}
+        key={`p_${pageID}`}
         shouldUpdate={true}
         render={this.props.renderPage.bind(
           null,
@@ -287,8 +262,7 @@ class ViewPager extends PureComponent {
 
     let bodyComponents = [];
 
-    let pagesNum = 0;
-    let hasLeft = false;
+    let pagesNum = 1; // default center page is in.
     let viewWidth = this.state.viewWidth;
 
     if (pageIDs.length > 0 && viewWidth > 0) {
@@ -296,23 +270,14 @@ class ViewPager extends PureComponent {
       if (this.state.currentPage > 0) {
         bodyComponents.push(this._getPage(this.state.currentPage - 1));
         pagesNum++;
-        hasLeft = true;
-      } else if (this.state.currentPage == 0 && this.props.isLoop) {
-        bodyComponents.push(this._getPage(pageIDs.length - 1, true));
-        pagesNum++;
-        hasLeft = true;
       }
 
       // center page
       bodyComponents.push(this._getPage(this.state.currentPage));
-      pagesNum++;
 
       // right page
       if (this.state.currentPage < pageIDs.length - 1) {
         bodyComponents.push(this._getPage(this.state.currentPage + 1));
-        pagesNum++;
-      } else if (this.state.currentPage == pageIDs.length - 1 && this.props.isLoop) {
-        bodyComponents.push(this._getPage(0, true));
         pagesNum++;
       }
     }
@@ -328,15 +293,16 @@ class ViewPager extends PureComponent {
     });
 
     return (
-      <View style={{ flex: 1 }}
+      <View
+        style={{ flex: 1 }}
         onLayout={(event) => {
           let viewWidth = event.nativeEvent.layout.width;
           if (!viewWidth || this.state.viewWidth === viewWidth) {
             return;
           }
           this.setState({
+            viewWidth,
             currentPage: this.state.currentPage,
-            viewWidth: viewWidth,
           });
         }}
       >
@@ -348,17 +314,5 @@ class ViewPager extends PureComponent {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  indicators: {
-    flex: 1,
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 10,
-    left: 0,
-    right: 0,
-    backgroundColor: 'transparent',
-  },
-});
 
 export default ViewPager;
