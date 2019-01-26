0ec5a9dcb134eb5000f2c066d134bc5726762669
czq <chenzhengqiang@youzan.com>
2019-01-26 14:18:08 +0800

    fix: link

--- android/app/src/main/java/com/tassel/MainApplication.java
+++ android/app/src/main/java/com/tassel/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage()
       );
