90828577e5037220095bea4eb51f135656905093
lear <1325925707@qq.com>
2019-07-28 22:49:57 +0800

    fx: 更新server

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const serverIps = [
   'https://5hmmnxt7ywzh.cn-e1.leanapp.cn',
-  'https://tasselstorage2.cn-e1.leanapp.cn'
+  'https://m5kvxu6mkpsy.cn-e1.leanapp.cn'
 ];
 
 export const searchIp = 'https://koapi.leanapp.cn';
