40f7bcc470d590b2cf34d5f2c19a854ee44ead12
lear <1325925707@qq.com>
2019-08-28 20:54:10 +0800

    fix: use autolink

--- android/app/src/main/java/com/tassel/MainApplication.java
+++ android/app/src/main/java/com/tassel/MainApplication.java
@@ -3,11 +3,6 @@ package com.tassel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import org.capslock.RNDeviceBrightness.RNDeviceBrightness;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,12 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new AsyncStoragePackage(),
-            new VectorIconsPackage(),
-            new SplashScreenReactPackage(),
-            new RNGestureHandlerPackage(),
-            new RNDeviceBrightness()
+          new MainReactPackage()
       );
     }
 
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-largelist': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
+};
